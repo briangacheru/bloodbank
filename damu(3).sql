41647f941988246823f7716becce6abd43df97d8
iambriangacheru <briangacheru14@gmail.com>
2017-06-14 21:23:03 +0300

    updated database file

--- damu(3).sql
+++ damu(3).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2017 at 05:58 AM
+-- Generation Time: Jun 14, 2017 at 08:21 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.5
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `fname`, `lname`, `email`, `password`, `ppic`, `username`) VALUES
-(6, 'lil', 'admin', 'admin@mail.com', '$2y$10$mhQhm1iurXF6SVuhpxr1YeedduORYS.p7hDd4/nlyEkxgzz0jX9ai', '375930.jpg', ''),
+(6, 'lil', 'admin', 'admin@mail.com', '$2y$10$OVwjzd.gcnF7dUytz/HU0uMTc9IIVjJU0C0wnjB4AgEKU3KL2ugpq', '940368.jpg', ''),
 (7, NULL, NULL, NULL, '$2y$10$lC4gXCFBKVE6bM/WKRpBcumn8SZHLpRTyhSFB28v5Ty2recavsLru', '', 'admin');
 
 -- --------------------------------------------------------
@@ -123,7 +123,29 @@ CREATE TABLE `camps` (
 
 INSERT INTO `camps` (`id`, `title`, `body`, `author`, `odate`, `cdate`) VALUES
 (9, 'Moi University', 'Eldoret', 'Administrator', '2016-12-31', '2017-01-14'),
-(10, 'Kesses', 'Soy', 'Ninja', '2017-03-29', '2017-04-12');
+(10, 'Kesses', 'Soy', 'Ninja', '2017-03-29', '2017-04-12'),
+(24, 'Trial', 'Eldoret', 'Ninja', '2017-05-31', '2017-06-21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `message` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`id`, `name`, `email`, `message`) VALUES
+(1, 'real', 'real@mail.com', 'ghgh'),
+(2, 'Brian Gacheru', 'bryo4419@gmail.com', 'hghjkl;kjhgfdrdtfygujhkl');
 
 -- --------------------------------------------------------
 
@@ -146,7 +168,7 @@ CREATE TABLE `doctorregister` (
 --
 
 INSERT INTO `doctorregister` (`id`, `fname`, `lname`, `code`, `email`, `password`, `ppic`) VALUES
-(11, 'real', 'doctor', 'DOC123', 'doc@mail.com', '$2y$10$PBQPf1wVHcleZO2Et5dA5e0lNMmvnDK5oDRj7t4VfrQFQQPxNJWNu', '149929.jpg');
+(11, 'real', 'doctor', 'DOC123', 'doc@mail.com', '$2y$10$/MVZMU2sr9BzDunqsVjd8.12fjQCAfgqvb6UHykDLFxEwHrbzk.8y', '581291.png');
 
 -- --------------------------------------------------------
 
@@ -178,21 +200,24 @@ CREATE TABLE `donarregister` (
 --
 
 INSERT INTO `donarregister` (`id`, `fname`, `lname`, `gender`, `b_id`, `ppic`, `day`, `month`, `year`, `county`, `mobile`, `email`, `password`, `last_donation_date`, `last_donation_camp`, `number_of_donations`) VALUES
-(9, 'Brian', 'Gacheru', 'Male', 'O-', '457056.jpg', 30, 'October', 1995, '', 710301320, 'bryo4419@gmail.com', '$2y$10$3slXWI6f28TVnsUcrLr6ReJP8RaE3jMX.GroT/ee/.8/kGCbBpCZy', '2016-03-30', 'Eldoret', 5),
+(9, 'Brian', 'Gacheru', 'Male', 'O-', '74382.png', 30, 'October', 1995, 'Uasin Gishu', 710301320, 'bryo4419@gmail.com', '$2y$10$3slXWI6f28TVnsUcrLr6ReJP8RaE3jMX.GroT/ee/.8/kGCbBpCZy', '2016-03-30', 'Eldoret', 5),
 (10, 'Peinah', 'Jepkorir', 'Female', 'B+', '728239.jpg', 22, 'December', 1995, '', 729318034, 'peinah@mail.com', '$2y$10$gND8bnkcYOSrUJkVzmFalONP.oziWO7HF5ikq2UEwhSp86WPrmf5u', '2016-04-14', 'Moi University', 4),
 (14, 'lil', 'ninja', 'Female', 'A-', '879607.jpg', 17, 'July', 1989, '', 729318067, 'lil@mail.com', '$2y$10$.akNwCFiTwp6xTiNlt5TdeJAxFGiIy6j4TxK6iKhaFz1.3QjewbVm', '2015-06-25', 'Moi University', 4),
-(15, 'real', 'user', 'Female', 'AB-', '', 17, 'February', 1990, '', 729318009, 'user1@mail.com', '$2y$10$ZIfUEOmdABC0jAqAeAgsM.aW/RQXVaCZDQN8mHLdcjB1rOnZVVene', '2016-07-21', 'Kesses', 6),
+(15, 'real', 'user', 'Female', 'AB-', '', 17, 'February', 1990, 'Nairobi', 729318009, 'user1@mail.com', '$2y$10$ZIfUEOmdABC0jAqAeAgsM.aW/RQXVaCZDQN8mHLdcjB1rOnZVVene', '2016-07-21', 'Kesses', 6),
 (16, 'julie', 'wambui', 'Female', 'B-', '71608.jpg', 19, 'December', 1995, '', 705701719, 'julie@mail.com', '$2y$10$OL3ZY1hdIJ9xCdrwczAZq.Nndff26V4tWsVXT/rv3/mvP0tIcV/gG', '2015-10-19', 'Kesses', 3),
 (18, 'Yule', 'Msee', 'Female', 'AB+', '565255.jpg', 1, 'January', 1993, '', 710301323, 'msee@mail.com', '$2y$10$XSmFUqcceZDnmAY2p/buduTIDs3jqhhaGiqkuA4TNDLWyjymgxRI2', '2017-01-16', 'Kesses', 6),
 (19, 'Donor', 'Ninja', 'Female', 'O-', '869180.jpg', 10, 'April', 1995, '', 705107916, 'donor@mail.com', '$2y$10$kV57o9DlJXrAJ8GmDu0Wf.pDx0R.sc93Ng6jB7vzaY9eP0Hqg7vXC', '2017-01-16', 'Moi University', 3),
 (20, 'Latifa', 'Lee', 'Female', 'O+', '873083.jpg', 30, 'November', 1997, '', 710301326, 'lee@mail.com', '$2y$10$yOhLpg1gUwdQLonQBC7BG.NArhAnSR8Q5G5Of0Ft/dPZH.Z4mzB96', '2017-02-15', 'Eldoret', 6),
 (21, 'Don', 'Don', 'Female', 'A+', '382515.jpg', 1, 'January', 1993, '', 710301329, 'don@mail', '$2y$10$Yr0MBUkjbidSLltBjhQm0eNQiUAcakvs6A0AcP7tSGPgmFxL4nGYK', '2017-02-15', 'Town', 5),
 (22, 'Developer', 'Carol', 'Female', 'B-', '3652.png', 16, 'October', 1996, '', 710342342, 'developer@mail.com', '$2y$10$XSkSN2m.EaX6J1opbb7Ahecdm8sQGdiWr9.COzOyJTJwfgqg2UyWO', '2017-04-16', 'Kesses', 6),
-(23, 'Freezie', 'Humphrey', 'Male', 'AB+', '396112.png', 28, 'February', 1993, '', 708720092, 'humphreyb@appslab.co.ke', '$2y$10$QeFdSidV/9dwlv1k6//gae2DRnzjhA3o2cT8PLlKopI68mk0RA4sC', '2017-04-04', 'mukuru', 1),
+(23, 'Freezie', 'Humphrey', 'Male', 'O+', '396112.png', 28, 'February', 1993, '', 708720092, 'humphreyb@appslab.co.ke', '$2y$10$QeFdSidV/9dwlv1k6//gae2DRnzjhA3o2cT8PLlKopI68mk0RA4sC', '2017-06-12', 'mukuru', 1),
 (27, 'The', 'Operator', 'Male', 'O-', '', 1, 'January', 1990, 'Kiambu', 710301328, 'op@mail.com', '$2y$10$lSMUPjPYfum6tcb90smeMuC2mpknSBW.Vmte9pnH/R/bVXzhOecUK', '2017-04-16', 'Moi University', 5),
-(28, 'Manu', 'El', 'Male', '', '', 4, 'March', 1993, 'Uasin Gishu', 710301334, 'manu@mail.com', '$2y$10$uf1R/oTg7D9E4TN3BjqI2e./0cLv98jSk2mHleRfhPF9YRskzb.Qy', '0000-00-00', '', 0),
-(29, 'Siali', 'Gibs', 'Male', '', '450837.png', 1, 'January', 2000, 'Kiambu', 789898989, 'silali@gmail.com', '$2y$10$aASgXNmjwvV7YefpLAMHx.LEnEhxS/LMMYvgXaqs5m9Ksy51AQyQK', '0000-00-00', '', 0),
-(30, 'Manu', 'gEEK', 'Male', 'o-', '', 1, 'January', 1994, 'Machakos', 712909090, 'mail@mail.com', '$2y$10$NkUrbCZbSAEybM9zQjMtIOsqnfokYyH9BaEBEz7N9Um02MnEQkgN2', '0000-00-00', '', 0);
+(28, 'Manu', 'El', 'Male', 'A-', '', 4, 'March', 1993, 'Uasin Gishu', 710301334, 'manu@mail.com', '$2y$10$uf1R/oTg7D9E4TN3BjqI2e./0cLv98jSk2mHleRfhPF9YRskzb.Qy', '2017-06-14', 'Grace Chapel', 4),
+(29, 'Siali', 'Gibs', 'Male', 'A+', '450837.png', 1, 'January', 2000, 'Kiambu', 789898989, 'silali@gmail.com', '$2y$10$aASgXNmjwvV7YefpLAMHx.LEnEhxS/LMMYvgXaqs5m9Ksy51AQyQK', '2017-06-14', 'Grace Chapel', 3),
+(30, 'Manu', 'gEEK', 'Male', 'O-', '', 1, 'January', 1994, 'Machakos', 712909090, 'mail@mail.com', '$2y$10$NkUrbCZbSAEybM9zQjMtIOsqnfokYyH9BaEBEz7N9Um02MnEQkgN2', '2017-06-14', 'Grace Chapel', 3),
+(31, 'Oma', 'Rion', 'Male', 'AB-', '852199.jpg', 14, 'September', 1990, 'Nairobi', 710301313, 'omarion@mail.com', '$2y$10$JFZh5afW8wY.O68CsJyiguSbRHD/UqzBm5Vux3aqXOLk1WuE0AuiO', '2017-06-14', 'Grace Chapel', 4),
+(32, 'Jane', 'Dough', 'Female', 'O-', '', 0, '', 0, '', 710301327, '', '', '0000-00-00', '', 0),
+(33, 'John', 'Doe', 'Male', 'A-', '', 0, '', 0, '', 710301313, '', '', '0000-00-00', '', 0);
 
 -- --------------------------------------------------------
 
@@ -214,8 +239,6 @@ CREATE TABLE `donation` (
 --
 
 INSERT INTO `donation` (`id`, `bloodgroup`, `pints`, `camp`, `ddate`, `timestamp`) VALUES
-(5, 'AB-', 51, 'Kesses', '2017-04-23', '2017-04-23 17:37:34'),
-(6, 'O+', 28, 'Kesses', '2017-04-23', '2017-04-23 18:07:10'),
 (7, 'B-', 29, 'Kesses', '2017-04-24', '2017-04-24 02:20:06'),
 (8, 'O-', 26, 'Kesses', '2017-04-24', '2017-04-24 02:28:50'),
 (9, 'A+', 53, 'Kesses', '2017-04-24', '2017-04-24 02:29:13'),
@@ -223,7 +246,8 @@ INSERT INTO `donation` (`id`, `bloodgroup`, `pints`, `camp`, `ddate`, `timestamp
 (11, 'B+', 58, 'Kesses', '2017-04-24', '2017-04-24 02:30:17'),
 (12, 'AB+', 41, 'Kesses', '2017-04-24', '2017-04-24 02:30:37'),
 (13, 'B-', 35, 'Moi University', '2017-04-24', '2017-04-24 02:31:25'),
-(14, 'B+', 34, 'Grace', '2017-05-15', '2017-05-15 11:49:50');
+(15, 'O+', 45, 'Grace Chapel', '2017-06-01', '2017-06-14 16:32:33'),
+(16, 'AB-', 78, 'Grace Chapel', '2017-06-01', '2017-06-14 16:33:34');
 
 -- --------------------------------------------------------
 
@@ -259,7 +283,8 @@ INSERT INTO `requests` (`id`, `email`, `fname`, `lname`, `mobile`, `b_id`, `code
 (93, 'bryo4419@gmail.com', 'Brian', 'Gacheru', '710301320', 'O-', '', '2017-04-23', 'Received', '2017-04-28 03:13:38', 2, 2, '2017-04-23'),
 (96, 'developer@mail.com', 'Developer', 'Carol', '710342342', 'B-', '', '2017-05-01', 'Waiting', '2017-04-28 03:13:48', 4, 4, '2017-04-23'),
 (97, 'op@mail.com', 'The', 'Operator', '710301328', 'O-', 'DOC123', '2017-05-03', 'Waiting', '2017-04-28 03:13:55', 4, 4, '2017-04-23'),
-(98, 'mail@mail.com', 'Manu', 'gEEK', '712909090', 'o-', 'DOC123', '2017-05-05', 'Waiting', '2017-04-28 07:09:39', 5, 0, '0000-00-00');
+(98, 'mail@mail.com', 'Manu', 'gEEK', '712909090', 'o-', 'DOC123', '2017-05-05', 'Waiting', '2017-04-28 07:09:39', 5, 0, '0000-00-00'),
+(100, 'lee@mail.com', 'Latifa', 'Lee', '710301326', 'O+', '', '2017-06-21', 'Waiting', '2017-06-14 17:22:18', 1, 0, '0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -287,6 +312,12 @@ ALTER TABLE `bloodunits`
 -- Indexes for table `camps`
 --
 ALTER TABLE `camps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -336,7 +367,12 @@ ALTER TABLE `bloodunits`
 -- AUTO_INCREMENT for table `camps`
 --
 ALTER TABLE `camps`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `doctorregister`
 --
@@ -346,17 +382,17 @@ ALTER TABLE `doctorregister`
 -- AUTO_INCREMENT for table `donarregister`
 --
 ALTER TABLE `donarregister`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT for table `donation`
 --
 ALTER TABLE `donation`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
